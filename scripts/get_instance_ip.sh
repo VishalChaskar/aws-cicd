c9cf685e805dfaed784b8ffdbf8083b0d6e11f9f
Parth Bhagat <53949269+CrashNBurn1337@users.noreply.github.com>
2024-11-10 15:26:06 +0000

    Update get_instance_ip.sh

--- scripts/get_instance_ip.sh
+++ scripts/get_instance_ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Capture the EC2 instance's public IP from Terraform
-INSTANCE_IP=$(terraform output -raw instance_ip)
+# Get the public IP of the EC2 instance from Terraform output
+INSTANCE_IP=$(terraform output -raw vm_ip)
 
 # Check if the IP is available
 if [ -z "$INSTANCE_IP" ]; then
@@ -9,6 +9,9 @@ if [ -z "$INSTANCE_IP" ]; then
   exit 1
 fi
 
-# Save the IP to a file
+# Save the IP to a text file for use by other jobs
 echo "$INSTANCE_IP" > instance_ip.txt
 echo "EC2 Instance IP: $INSTANCE_IP"
+
+# Export the IP for use in subsequent steps
+export INSTANCE_IP
