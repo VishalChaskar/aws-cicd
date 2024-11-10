7eefa11cdff146380fafee5c90dd5544101c0607
Parth Bhagat <53949269+CrashNBurn1337@users.noreply.github.com>
2024-11-10 14:28:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ data "aws_key_pair" "existing_key" {
 
 # EC2 Instance using the key pair and security group
 resource "aws_instance" "vm" {
-  ami           = "ami-03ca36368dbc9cfa1"  # Replace with your preferred AMI ID
+  ami           = "ami-0917d3c16c89e5dc3"  # Replace with your preferred AMI ID
   instance_type = "t2.micro"
   key_name      = data.aws_key_pair.existing_key.key_name   # Reference the existing key pair
 
